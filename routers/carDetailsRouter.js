ca83701a859a7f2807df2265089f8594a7b6b0c4
Thomas Barrett <tsbarrett89@gmail.com>
2019-12-11 13:47:23 -0700

    added get and post operations to carDetailsRouter

--- routers/carDetailsRouter.js
+++ routers/carDetailsRouter.js
@@ -1,8 +1,24 @@
 const express = require('express');
-
 const db = require('../data/dbConfig.js');
-
 const router = express.Router();
 
-router.use(express.json());
+router.get('/', (req, res) =>{
+    db.select('*').from('car_details')
+        .then(details => {
+            res.status(200).json(details);
+        })
+        .catch(err => res.status(500).json({errorMessage:"error getting car details"}))
+})
 
+router.post('/', (req, res) => {
+    const carDetails = req.body;
+
+    db('car_details').insert(carDetails, 'id')
+        .then(details => {
+            res.status(201).json(details);
+        })
+        .catch(err => res.status(500).json({errorMessage:"error adding car details"}))
+})
+
+
+module.exports = router
