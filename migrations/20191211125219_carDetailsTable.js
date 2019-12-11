444c65d2fba763e177f9fbfc23e42272c9a70969
Thomas Barrett <tsbarrett89@gmail.com>
2019-12-11 13:11:26 -0700

    created carDetails schema, also added .env and routers folder

--- migrations/20191211125219_carDetailsTable.js
+++ migrations/20191211125219_carDetailsTable.js
@@ -1,6 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('carDetails', tbl => {
+      tbl.increments();
+      tbl.string('VIN').notNullable();
+      tbl.string('make').notNullable();
+      tbl.string('model').notNullable();
+      tbl.integer('mileage').notNullable();
+      tbl.string('transmission_type');
+      tbl.string('title_status');
+  })
 };
 
 exports.down = function(knex) {
